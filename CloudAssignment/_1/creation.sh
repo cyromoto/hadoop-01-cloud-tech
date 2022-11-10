b6da65c879731986472c8e85c238b740ca519195
Yakoob Youseph Cyrus <yakoobcyrus@gmail.com>
2022-11-10 23:00:58 +0000

    --hive queries for cleaning

--- CloudAssignment/_1/creation.sh
+++ CloudAssignment/_1/creation.sh
@@ -1,13 +1,91 @@
-gcloud dataproc clusters create cluster-5970 
-    --enable-component-gateway 
-    --bucket hadoop-data-bucket-01 
-    --region us-central1 
-    --zone us-central1-c 
-    -master-machine-type n1-standard-4 
-    --master-boot-disk-size 100 
-    --num-workers 2 
-    --worker-machine-type n1-standard-4 
-    --worker-boot-disk-size 30 
-    --image-version 2.0-debian10 
-    --max-idle 7200s 
-    --project hadoop-assignment-101
+#creation of dataproc with two worker nodes
+
+gcloud dataproc clusters create cluster-9c40 
+--enable-component-gateway 
+--region us-central1 
+--zone us-central1-f 
+--master-machine-type n1-standard-2 
+--master-boot-disk-size 100 
+--num-workers 2 
+--worker-machine-type n1-standard-2 
+--worker-boot-disk-size 50 
+--image-version 2.0-debian10 
+--scopes 'https://www.googleapis.com/auth/cloud-platform' 
+--project hadoop-assignment-101
+
+POST /v1/projects/hadoop-assignment-101/regions/us-central1/clusters/
+{
+  "projectId": "hadoop-assignment-101",
+  "clusterName": "cluster-9c40",
+  "config": {
+    "configBucket": "",
+    "gceClusterConfig": {
+      "serviceAccountScopes": [
+        "https://www.googleapis.com/auth/cloud-platform"
+      ],
+      "networkUri": "default",
+      "subnetworkUri": "",
+      "internalIpOnly": false,
+      "zoneUri": "us-central1-f",
+      "metadata": {},
+      "tags": [],
+      "shieldedInstanceConfig": {
+        "enableSecureBoot": false,
+        "enableVtpm": false,
+        "enableIntegrityMonitoring": false
+      }
+    },
+    "masterConfig": {
+      "numInstances": 1,
+      "machineTypeUri": "n1-standard-2",
+      "diskConfig": {
+        "bootDiskType": "pd-standard",
+        "bootDiskSizeGb": "100",
+        "numLocalSsds": 0,
+        "localSsdInterface": "SCSI"
+      },
+      "minCpuPlatform": "",
+      "imageUri": ""
+    },
+    "softwareConfig": {
+      "imageVersion": "2.0-debian10",
+      "properties": {},
+      "optionalComponents": []
+    },
+    "lifecycleConfig": {},
+    "initializationActions": [],
+    "encryptionConfig": {
+      "gcePdKmsKeyName": ""
+    },
+    "autoscalingConfig": {
+      "policyUri": ""
+    },
+    "endpointConfig": {
+      "enableHttpPortAccess": true
+    },
+    "securityConfig": {
+      "kerberosConfig": {}
+    },
+    "workerConfig": {
+      "numInstances": 2,
+      "machineTypeUri": "n1-standard-2",
+      "diskConfig": {
+        "bootDiskType": "pd-standard",
+        "bootDiskSizeGb": "50",
+        "numLocalSsds": 0,
+        "localSsdInterface": "SCSI"
+      },
+      "minCpuPlatform": "",
+      "imageUri": ""
+    },
+    "secondaryWorkerConfig": {
+      "numInstances": 0
+    }
+  },
+  "labels": {},
+  "status": {},
+  "statusHistory": [
+    {}
+  ],
+  "metrics": {}
+}
---
+++ CloudAssignment/_3/cleaning.hql
@@ -0,0 +1,46 @@
+--table creation from hdfs file
+CREATE EXTERNAL TABLE musicset 
+(marketplace STRING, 
+      customer_id DOUBLE, 
+      review_id STRING,
+      product_id STRING,
+      product_parent DOUBLE,
+      product_title STRING,
+      product_category STRING,
+      helpful_votes INT,
+	  star_rating INT,
+      total_votes INT,
+      vine STRING,  
+      verified_purchase STRING,
+      review_headline STRING,
+      review_body STRING,
+      review_date DATE
+      )
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '\t'
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE 
+LOCATION '/dataset'
+TBLPROPERTIES("skip.header.line.count"="1");
+
+
+--test if table is created
+SELECT * FROM musicset LIMIT 10;
+
+--creating temp table for cleaning
+ALTER TABLE musicset RENAME TO table_to_dump;
+
+
+--removal of null values and the removal of unwanted character from the body 
+CREATE TABLE musicset AS
+SELECT regexp_replace(review_id,"[$&+,:;=?@#|<>.^*()%!-]"," "),
+    regexp_replace(product_id,"[$&+,:;=?@#|<>.^*()%!-]"," "),
+    regexp_replace(review_body,"[$&+,:;=?@#|<>.^*()%!-]"," "),
+    regexp_replace(review_headline,"[$&+,:;=?@#|<>.^*()%!-]"," "),
+    regexp_replace(helpful_votes,"[$&+,:;=?@#|<>.^*()%!-]"," ")
+FROM table_to_dump 
+WHERE review_id IS NOT NULL 
+    and product_id IS NOT NULL 
+    and review_body IS NOT NULL
+    and review_headline IS NOT NULL
+    and helpful_votes IS NOT NULL;
